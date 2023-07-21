f03b6c371f998e21e704395df686e59ec217b122
SilviyaTaseva <silviya.r.taseva@gmail.com>
2023-07-21 22:12:17 +0300

    Initial commit

---
+++ src/main/java/Homework_3/Homework_3/Homework_3_part_1.java
@@ -0,0 +1,23 @@
+package Homework_3;
+import java.util.Scanner;
+
+public class Homework_3_part_1
+{
+    public static void main(String[] args)
+
+    {
+        int a,b,c,largest;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        a= sc.nextInt();
+        System.out.println("Enter the second number: ");
+        b=sc.nextInt();
+        System.out.println("Enter the third number: ");
+        c= sc.nextInt();
+        largest=c>(a>b?a:b)?c:((a>b)?a:b);
+        System.out.println("The largest number is: "+largest);
+
+
+
+}
+}
---
+++ src/main/java/Homework_3/Homework_3/Homework_3_part_2.java
@@ -0,0 +1,28 @@
+package Homework_3;
+import java.util.Scanner;
+
+public class Homework_3_part_2
+{
+    // Лице и периметър на правоъгълник
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Въведете височина на правоъгълник: ");
+        int heght = sc.nextInt();
+        System.out.println("Въведете дължина на правоъгълник: ");
+        int lenght = sc.nextInt();
+        int areа = heght * lenght;
+        int perimeter = 2 * (heght + lenght);
+        System.out.println("Лицето на правоъгълника е :" + areа);
+        System.out.println("Периметърът на правоъгилника е :" + perimeter);
+    // Even or odd
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num= reader.nextInt();
+        if (num % 2 == 0)
+            System.out.println(num + " " + "it's even.");
+        else
+            System.out.println(num + " " + "it's odd.");
+
+
+    }
+    }
