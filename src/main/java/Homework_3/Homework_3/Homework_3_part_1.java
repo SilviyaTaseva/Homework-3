a6ef86d00908769ad282f817cd14b58b3ba21365
SilviyaTaseva <silviya.r.taseva@gmail.com>
2023-09-02 12:40:49 +0300

    Homework Projects

--- src/main/java/Homework_3/Homework_3/Homework_3_part_1.java
+++ src/main/java/Homework_3/Homework_3/Homework_3_part_1.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 
 public class Homework_3_part_1
 {
+//Напишете програма/ метод, която приема от конзолата 3 подадени числа и определя кое е най- голямото.
     public static void main(String[] args)
-
     {
+
         int a,b,c,largest;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the first number: ");
+         System.out.println("Enter the first number: ");
         a= sc.nextInt();
-        System.out.println("Enter the second number: ");
+
+         System.out.println("Enter the second number: ");
         b=sc.nextInt();
-        System.out.println("Enter the third number: ");
+
+         System.out.println("Enter the third number: ");
         c= sc.nextInt();
+
         largest=c>(a>b?a:b)?c:((a>b)?a:b);
-        System.out.println("The largest number is: "+largest);
-
-
-
+         System.out.println("The largest number is: "+largest);
 }
 }
